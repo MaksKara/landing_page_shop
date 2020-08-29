0baa0b07a330dceaae1fbad536d7f2414f6f15ec
MaksKara <62358244+MaksKara@users.noreply.github.com>
2020-08-29 21:53:18 +0300

    BANNER!

---
+++ js/slider.js
@@ -0,0 +1,53 @@
+const slides = document.querySelectorAll('.banner__slide'),
+	  dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+const activeSlide = n => {
+	for(slide of slides) {
+		slide.classList.remove('active');
+	}
+	slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+	for(dot of dots) {
+		dot.classList.remove('active');
+	}
+	dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+	activeSlide(ind);
+	activeDot(ind);
+}
+
+const nextSlide = () => {
+	if(index == slides.length - 1) {
+		index = 0;
+		prepareCurrentSlide(index);
+	} else {
+		index++;
+		prepareCurrentSlide(index);
+	}
+}
+
+const prevSlide = () => {
+	if(index == 0) {
+		index == slides.length - 1;
+		prepareCurrentSlide(index);
+	} else {
+		index--;
+		prepareCurrentSlide(index);
+	}
+}
+
+dots.forEach((item, indexDot) => {
+	item.addEventListener('click', () => {
+		index = indexDot;
+		prepareCurrentSlide(index);
+		clearInterval(interval);
+	});
+});
+
+const interval = setInterval(nextSlide, 5000);
