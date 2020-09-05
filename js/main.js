2626483aba922336b76b8e6fe75750dc4d719d9b
MaksKara <62358244+MaksKara@users.noreply.github.com>
2020-09-05 19:14:20 +0300

    products (js)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+let products = [
+{id: 1, title: 'Branded Shoe', price: 300, img: 'assets/img/products/product_1.png'},
+{id: 2, title: 'Branded Tees', price: 250, img: 'assets/img/products/product_2.png'},
+{id: 3, title: 'Branded Shoe', price: 350, img: 'assets/img/products/product_3.png'},
+{id: 4, title: 'Branded Shoe', price: 325, img: 'assets/img/products/product_4.png'},
+{id: 5, title: 'EMS Woman Bag', price: 275, img: 'assets/img/products/product_5.png'},
+{id: 6, title: 'Branded Cargos', price: 225, img: 'assets/img/products/product_6.png'}
+];
+
+let price = document.querySelector('.header__price'),
+	dotQuant = document.querySelector('.header__quant');
+
 window.onscroll = function() {
-	scrollFunc()
+	scrollFunc();
 };
 
 function scrollFunc() {
@@ -12,3 +24,26 @@ function scrollFunc() {
 		header.classList.remove('active');
 	}
 }
+
+price.innerHTML = '0$';
+dotQuant.innerHTML = 0;
+
+const drawHTML = product => `
+	<div class="col-lg-3 offset-lg-1">
+		<div class="products__item">
+			<img src="${product.img}" alt="${product.title}" class="products__img">
+			<p class="products__name">${product.title}</p>
+			<div class="products__cost">
+				<p class="products__price">${product.price}</p>
+				<a href="#" class="products__btn">Buy now</a>
+			</div>
+		</div>
+	</div>
+`
+
+function render() {
+	const html = products.map(drawHTML).join('');
+	document.querySelector('#products__cards').innerHTML = html;
+}
+
+render();
